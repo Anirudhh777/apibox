9fa14b2ade2d4df4c20e5fe3979ae583c252249c
Anirudhh777 <anirudhh1988@gmail.com>
2020-04-07 17:54:24 +0530

    start.sh bash script

---
+++ start.sh
@@ -0,0 +1,21 @@
+set -e
+
+[ -f ".env.prod" ]
+
+export $(cat .env.prod | grep -v ^# | xargs);
+echo Starting services
+docker-compose up -d
+echo Host: 127.0.0.1
+until docker-compose exec mysql mysql -h 127.0.0.1 -u $DB_USERNAME -p$DB_PASSWORD -D $DB_DATABASE --silent -e "show databases;"
+do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+echo Installing dependencies
+
+rm -f bootstrap/cache/*.php
+
+docker run --rm -v $(pwd):/app composer install
+echo php artisan migrate && echo Database migrated
+echo php artisan db:seed && echo Database seeded
+echo php artisan serve --port=8080
